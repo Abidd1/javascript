4814891f64df0ee5d9e79dd5ef9c3b47d2d9e813
Abid Ali <abidali129418@gmail.com>
2025-01-22 07:47:50 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+// Task first start from here 
+let marks = prompt("Enter your marks");
+if( marks >= 20){
+    alert("Your grade is C")
+}
+if( marks >= 40){
+    alert("Your grade is B")
+}
+if( marks >= 60){
+    alert("Your grade is B+")
+}
+if( marks >= 80){
+    alert("Your grade is A")
+}
+if( marks >= 90){
+    alert("Your grade is A+")
+}
+else{
+    alert("Please Enter your marks under 100")
+}
