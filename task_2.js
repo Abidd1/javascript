f74a26bee50f600827738f6c6980bfe2b1eb0316
Abid Ali <abidali129418@gmail.com>
2025-01-30 14:48:26 -0800

    Add files via upload

---
+++ task_2.js
@@ -0,0 +1,10 @@
+// Task two start from here!
+let temp = prompt("Enter Current tempreature")
+if(temp <= 36){
+    alert("Enjoy your day its nice")
+}
+else{
+    alert("Kor ki kena zanla de hasi kar katala da")
+}
+
+// Task Two //ends Here!
