e34b2d4e35113a940e60623b850981b857f78c19
Abid Ali <abidali129418@gmail.com>
2025-01-30 05:57:35 -0800

    Add files via upload

---
+++ task_1.js
@@ -0,0 +1,14 @@
+// Task firt start from here!
+
+let name = prompt("Enter Your name");
+let age = prompt("Enter your age");
+alert("Congrats you're a Human")
+
+// Task first ends here!
+
+
+
+
+
+
+
