fa02714220ac397bba395e9f3139d95b9aa5dada
Abid Ali <abidali129418@gmail.com>
2025-02-06 21:33:48 -0800

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,23 @@
+// Creating a JavaScript file practice 
+let marks = prompt("Enter your marks")
+
+
+if (marks>=90 ){
+    alert("Grade A+")
+}
+
+else(marks <= 80){
+    alert( "Grade A")
+}
+
+else if (marks >= 70){
+    alert("Grade B+")
+}
+ 
+else if (marks >= 60){
+    alert("Grade c+")
+}
+
+else if (marks >= 50){
+    alert("Grade c+")
+}
