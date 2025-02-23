ed2b18b915bf32780a994f2d743f80eb96a25c9d
Abid Ali <abidali129418@gmail.com>
2025-02-23 09:56:30 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,2 @@
-// Jaavascript start foem here
-
-let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-for (let i = 0; i < numbers.length; i++) {
-
-  let isEvenFlag = false;
-
-  if (numbers[i] % 2 === 0) {
-    isEvenFlag = true;
-  }
-
-  if (isEvenFlag) {
-    console.log(numbers[i] + 
-
-" is even");
-  } else {
-    console.log(numbers[i] + " is odd");
-  }
-}
+let milisec = newDate(2000-01-10);
+let sec = Math.floor
