08f7d0679202e83a858403a07992bee76d2d5509
Abid Ali <abidali129418@gmail.com>
2025-03-16 10:54:02 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-import {onCLS, onINP, onLCP} from 'web-vitals';
+// import {onCLS, onINP, onLCP} from 'web-vitals';
 
-function sendToAnalytics(metric) {
-  const body = JSON.stringify(metric);
-  // Use `navigator.sendBeacon()` if available, falling back to `fetch()`.
-  (navigator.sendBeacon && navigator.sendBeacon('/analytics', body)) ||
-    fetch('/analytics', {body, method: 'POST', keepalive: true});
+// function sendToAnalytics(metric) {
+//   const body = JSON.stringify(metric);
+//   // Use `navigator.sendBeacon()` if available, falling back to `fetch()`.
+//   (navigator.sendBeacon && navigator.sendBeacon('/analytics', body)) ||
+//     fetch('/analytics', {body, method: 'POST', keepalive: true});
+// }
+
+// onCLS(sendToAnalytics);
+// onINP(sendToAnalytics);
+// onLCP(sendToAnalytics);
+function checkAddress()
+{
+  let email = document.getElementById("email").value;
+  if (email === ""){
+    alert("email required")
+    return false;
+  }
 }
-
-onCLS(sendToAnalytics);
-onINP(sendToAnalytics);
-onLCP(sendToAnalytics);
